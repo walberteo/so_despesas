e6429cff05518d5337382e46663048b77b07428b
Walber Teodoro <walberteo@gmail.com>
2020-06-18 14:27:18 -0300

    Iniciado projeto

---
+++ android/app/src/main/kotlin/com/example/so_despesas/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.so_despesas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() => runApp(GetMaterialApp(home: MyHomePage()));
